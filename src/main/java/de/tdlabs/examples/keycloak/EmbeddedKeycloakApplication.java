78636a62644d85a7a1236b600bc23f7aa23d53c3
Thomas Darimont <thomas.darimont@gmail.com>
2018-09-13 22:38:58 +0200

    Update keycloak version to 4.4.0.Final

--- src/main/java/de/tdlabs/examples/keycloak/EmbeddedKeycloakApplication.java
+++ src/main/java/de/tdlabs/examples/keycloak/EmbeddedKeycloakApplication.java
@@ -5,6 +5,8 @@ import org.jboss.resteasy.core.Dispatcher;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.services.managers.ApplianceBootstrap;
 import org.keycloak.services.resources.KeycloakApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.core.Context;
@@ -17,56 +19,58 @@ import java.util.Arrays;
  */
 public class EmbeddedKeycloakApplication extends KeycloakApplication {
 
-  static KeycloakServerProperties keycloakServerProperties;
+    private static final Logger LOG = LoggerFactory.getLogger(EmbeddedKeycloakApplication.class);
 
-  public EmbeddedKeycloakApplication(@Context ServletContext context, @Context Dispatcher dispatcher) {
+    static KeycloakServerProperties keycloakServerProperties;
 
-    super(augmentToRedirectContextPath(context), dispatcher);
+    public EmbeddedKeycloakApplication(@Context ServletContext context, @Context Dispatcher dispatcher) {
 
-    tryCreateMasterRealmAdminUser();
-  }
+        super(augmentToRedirectContextPath(context), dispatcher);
 
-  private void tryCreateMasterRealmAdminUser() {
-
-    KeycloakSession session = getSessionFactory().create();
-
-    ApplianceBootstrap applianceBootstrap = new ApplianceBootstrap(session);
-
-    AdminUser admin = keycloakServerProperties.getAdminUser();
-
-    try {
-      session.getTransactionManager().begin();
-      applianceBootstrap.createMasterRealmUser(admin.getUsername(), admin.getPassword());
-      session.getTransactionManager().commit();
-    } catch (Exception ex) {
-      System.out.println("Couldn't create keycloak master admin user: " + ex.getMessage());
-      session.getTransactionManager().rollback();
+        tryCreateMasterRealmAdminUser();
     }
 
-    session.close();
-  }
+    private void tryCreateMasterRealmAdminUser() {
+
+        KeycloakSession session = getSessionFactory().create();
+
+        ApplianceBootstrap applianceBootstrap = new ApplianceBootstrap(session);
+
+        AdminUser admin = keycloakServerProperties.getAdminUser();
+
+        try {
+            session.getTransactionManager().begin();
+            applianceBootstrap.createMasterRealmUser(admin.getUsername(), admin.getPassword());
+            session.getTransactionManager().commit();
+        } catch (Exception ex) {
+            LOG.warn("Couldn't create keycloak master admin user: {}", ex.getMessage());
+            session.getTransactionManager().rollback();
+        }
+
+        session.close();
+    }
 
 
-  private static ServletContext augmentToRedirectContextPath(ServletContext servletContext) {
+    private static ServletContext augmentToRedirectContextPath(ServletContext servletContext) {
 
-    ClassLoader classLoader = servletContext.getClassLoader();
-    Class[] interfaces = {ServletContext.class};
+        ClassLoader classLoader = servletContext.getClassLoader();
+        Class[] interfaces = {ServletContext.class};
 
-    InvocationHandler invocationHandler = (proxy, method, args) -> {
+        InvocationHandler invocationHandler = (proxy, method, args) -> {
 
-      if ("getContextPath".equals(method.getName())) {
-        return keycloakServerProperties.getContextPath();
-      }
+            if ("getContextPath".equals(method.getName())) {
+                return keycloakServerProperties.getContextPath();
+            }
 
-      if ("getInitParameter".equals(method.getName()) && args.length == 1 && "keycloak.embedded".equals(args[0])) {
-        return "true";
-      }
+            if ("getInitParameter".equals(method.getName()) && args.length == 1 && "keycloak.embedded".equals(args[0])) {
+                return "true";
+            }
 
-      System.out.printf("%s %s%n", method.getName(), Arrays.toString(args));
+            LOG.info("{} {}", method.getName(), Arrays.toString(args));
 
-      return method.invoke(servletContext, args);
-    };
+            return method.invoke(servletContext, args);
+        };
 
-    return ServletContext.class.cast(Proxy.newProxyInstance(classLoader, interfaces, invocationHandler));
-  }
+        return ServletContext.class.cast(Proxy.newProxyInstance(classLoader, interfaces, invocationHandler));
+    }
 }

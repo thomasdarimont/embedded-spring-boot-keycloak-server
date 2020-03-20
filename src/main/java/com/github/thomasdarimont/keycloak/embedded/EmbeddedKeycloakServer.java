d1ca9e09abf4bd46482fbfb76fefd68c20e33626
Thomas Darimont <thomas.darimont@googlemail.com>
2020-03-20 12:24:30 +0100

    Cleanup imports

--- src/main/java/com/github/thomasdarimont/keycloak/embedded/EmbeddedKeycloakServer.java
+++ src/main/java/com/github/thomasdarimont/keycloak/embedded/EmbeddedKeycloakServer.java
@@ -1,7 +1,6 @@
 package com.github.thomasdarimont.keycloak.embedded;
 
 import lombok.extern.jbosslog.JBossLog;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;

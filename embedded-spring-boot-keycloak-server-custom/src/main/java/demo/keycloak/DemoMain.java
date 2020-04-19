9fdec6eb9fd19c52668c811e33be1d7e6b3631d5
Thomas Darimont <thomas.darimont@googlemail.com>
2020-04-19 10:56:06 +0200

    Remove unnecessary throws clause in DemoMain

--- embedded-spring-boot-keycloak-server-custom/src/main/java/demo/keycloak/DemoMain.java
+++ embedded-spring-boot-keycloak-server-custom/src/main/java/demo/keycloak/DemoMain.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackageClasses = EmbeddedKeycloakServer.class)
 public class DemoMain {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(DemoMain.class, args);
     }
 }

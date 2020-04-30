f207e19c872fcdfc72488e47f077f0342f69a458
Jonas Bark <jonas.bark@ffuf.de>
2020-04-30 22:24:22 +0200

    fix unit test

--- embedded-keycloak-server-plain/src/test/java/com/github/thomasdarimont/keycloak/EmbeddedSpringBootKeycloakServerXApplicationTests.java
+++ embedded-keycloak-server-plain/src/test/java/com/github/thomasdarimont/keycloak/EmbeddedSpringBootKeycloakServerXApplicationTests.java
@@ -3,7 +3,7 @@ package com.github.thomasdarimont.keycloak;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class EmbeddedSpringBootKeycloakServerXApplicationTests {
 
     @Test

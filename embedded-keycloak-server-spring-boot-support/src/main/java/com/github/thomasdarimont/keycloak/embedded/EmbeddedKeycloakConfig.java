f5e95ddb2e78527cf30c28c15d0ce821439f3b32
Thomas Darimont <thomas.darimont@googlemail.com>
2020-05-14 21:11:48 +0200

    Disable Resteasy HTML sanitizer to avoid erroneous HTML encoding
    Fixes #14
    
    (cherry picked from commit fc469ed2f7497e1f9bf3e04f884106edac595004)

--- embedded-keycloak-server-spring-boot-support/src/main/java/com/github/thomasdarimont/keycloak/embedded/EmbeddedKeycloakConfig.java
+++ embedded-keycloak-server-spring-boot-support/src/main/java/com/github/thomasdarimont/keycloak/embedded/EmbeddedKeycloakConfig.java
@@ -74,6 +74,7 @@ public class EmbeddedKeycloakConfig {
 
         servlet.addInitParameter(ResteasyContextParameters.RESTEASY_SERVLET_MAPPING_PREFIX, customProperties.getServer().getKeycloakPath());
         servlet.addInitParameter(ResteasyContextParameters.RESTEASY_USE_CONTAINER_FORM_PARAMS, "false");
+        servlet.addInitParameter(ResteasyContextParameters.RESTEASY_DISABLE_HTML_SANITIZER, "true");
         servlet.addUrlMappings(customProperties.getServer().getKeycloakPath() + "/*");
         servlet.setLoadOnStartup(2);
         servlet.setAsyncSupported(true);

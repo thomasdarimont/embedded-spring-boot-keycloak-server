4f9affaf2804462bcd35bacaf96f1dd2e64acc80
Thomas Darimont <thomas.darimont@googlemail.com>
2020-06-30 23:00:16 +0200

    Polishing

--- embedded-keycloak-server-spring-boot-support/src/main/java/com/github/thomasdarimont/keycloak/embedded/support/PopulateKeycloakPropertiesApplicationListener.java
+++ embedded-keycloak-server-spring-boot-support/src/main/java/com/github/thomasdarimont/keycloak/embedded/support/PopulateKeycloakPropertiesApplicationListener.java
@@ -9,6 +9,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,28 +21,31 @@ import java.util.List;
 public class PopulateKeycloakPropertiesApplicationListener implements ApplicationListener<ApplicationEvent> {
 
     @Override
-    public void onApplicationEvent(ApplicationEvent event) {
-        if (event instanceof ApplicationEnvironmentPreparedEvent) {
-            ApplicationEnvironmentPreparedEvent envEvent = (ApplicationEnvironmentPreparedEvent) event;
-            ConfigurableEnvironment env = envEvent.getEnvironment();
+    public void onApplicationEvent(@NonNull ApplicationEvent event) {
 
-            try {
-                Resource resource = new ClassPathResource("keycloak-defaults.yml");
+        if (!(event instanceof ApplicationEnvironmentPreparedEvent)) {
+            return;
+        }
 
-                if (!resource.exists()) {
-                    return;
-                }
+        ApplicationEnvironmentPreparedEvent envEvent = (ApplicationEnvironmentPreparedEvent) event;
+        ConfigurableEnvironment env = envEvent.getEnvironment();
 
-                log.info("Loading default keycloak properties configuration from: {}", resource.getURI());
+        try {
+            Resource resource = new ClassPathResource("keycloak-defaults.yml");
 
-                YamlPropertySourceLoader sourceLoader = new YamlPropertySourceLoader();
-                List<PropertySource<?>> yamlTestProperties = sourceLoader.load("keycloak-defaults.yml", resource);
-                if (!yamlTestProperties.isEmpty()) {
-                    env.getPropertySources().addLast(yamlTestProperties.get(0));
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            if (!resource.exists()) {
+                return;
             }
+
+            log.info("Loading default keycloak properties configuration from: {}", resource.getURI());
+
+            YamlPropertySourceLoader sourceLoader = new YamlPropertySourceLoader();
+            List<PropertySource<?>> yamlTestProperties = sourceLoader.load("keycloak-defaults.yml", resource);
+            if (!yamlTestProperties.isEmpty()) {
+                env.getPropertySources().addLast(yamlTestProperties.get(0));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
